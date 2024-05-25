59cf925a8c698da8fc8f0b9fd2166d22accd8ab1
pakalamandasagar <pakalamandasagar955@gmail.com>
2024-05-25 14:02:27 +0530

    Installing mysql

--- 10-functions.sh
+++ 10-functions.sh
@@ -3,12 +3,12 @@
 ID=$(id -u)
 
 VALIDATE(){
-    if [ $? -ne 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo "ERROR:: Installing is failed"
+        echo "ERROR:: $2 ... FAILED"
         exit 1
     else
-        echo "Installing is SUCCESS"
+        echo "$2 ... SUCCESS"
     fi
 }
 
@@ -22,8 +22,12 @@ fi # fi means reverse of if, indicating codition end
 
 yum inatall MySQL -y
 
-VALIDATE
+VALIDATE $? "Installing MySQL"
 
 yum instal git -y
+
+VALIDATE $? "Installing GIT"
+
+
 
 VALIDATE
