cc0ff8123e3f7d7d3ed94fda1eb95eefc23f41ac
pakalamandasagar <pakalamandasagar955@gmail.com>
2024-05-25 13:22:20 +0530

    installing mysql

--- 09-install-mysqi.sh
+++ 09-install-mysqi.sh
@@ -5,8 +5,9 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROE:: please run this script with root access"
+    exit 1 # you can give other than 0
 else
     echo "you are root user"
-fi
+fi # fi means reverse of if, indicating codition end
 
 yum install mysql -y
