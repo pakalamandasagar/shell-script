4cae1545e09d738fe1bbf4e1d25cae6cde07eee0
pakalamandasagar <pakalamandasagar955@gmail.com>
2024-05-28 15:29:35 +0530

    added 2nd script

---
+++ 13-install-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROE:: please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating codition end
+
+echo "All arguments passed: $@"
